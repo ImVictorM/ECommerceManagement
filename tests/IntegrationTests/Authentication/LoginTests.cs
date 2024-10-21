7d84de192c4b137ab3a229680bdae626f1402039
ImVictorM <victorutdsim7@gmail.com>
2024-10-21 18:00:39 -0300

    test: add tests to the login process

--- tests/IntegrationTests/Authentication/LoginTests.cs
+++ tests/IntegrationTests/Authentication/LoginTests.cs
@@ -1,8 +1,10 @@
+using System.Net;
 using System.Net.Http.Json;
 using Contracts.Authentication;
 using FluentAssertions;
 using IntegrationTests.Common;
 using IntegrationTests.TestUtils.Seeds;
+using Microsoft.AspNetCore.Http;
 
 namespace IntegrationTests.Authentication;
 
@@ -19,13 +21,76 @@ public class LoginTests : BaseIntegrationTest
     {
     }
 
-    [Fact]
-    public async Task Login_WhenCredentialsAreValid_AuthenticateTheUserCorrectly()
+    /// <summary>
+    /// List of valid login request objects.
+    /// </summary>
+    /// <returns>A list of valid login requests.</returns>
+    public static IEnumerable<object[]> ValidRequests()
     {
-        var loginRequest = new LoginRequest(UserSeed.User1.Email.Value, UserSeed.UserPassword);
+        yield return new object[] { new LoginRequest(UserSeed.User1.Email.Value, UserSeed.UserPassword) };
+        yield return new object[] { new LoginRequest(UserSeed.User2.Email.Value, UserSeed.UserPassword) };
+        yield return new object[] { new LoginRequest(UserSeed.Admin.Email.Value, UserSeed.AdminPassword) };
+    }
 
-        var response = await HttpClient.PostAsJsonAsync("/auth/login", loginRequest);
+    /// <summary>
+    /// List of invalid login request objects.
+    /// </summary>
+    /// <returns>A list of invalid login requests.</returns>
+    public static IEnumerable<object[]> InvalidRequests()
+    {
+ 
 
-        response.Should().BeSuccessful();
+        yield return new object[] { new LoginRequest("", ""), "Email", "Password" };
+        yield return new object[] { new LoginRequest("          ", "        "), "Email", "Password" };
+        yield return new object[] { new LoginRequest(UserSeed.User1.Email.Value, ""), "Password" };
+        yield return new object[] { new LoginRequest("", UserSeed.UserPassword), "Email" };
+    }
+
+    /// <summary>
+    /// Checks if it is possible to login with valid credentials.
+    /// </summary>
+    /// <param name="loginRequest">The request object.</param>
+    /// <returns>An asynchronous operation.</returns>
+    [Theory]
+    [MemberData(nameof(ValidRequests))]
+    public async Task Login_WhenCredentialsAreValid_AuthenticateTheUserCorrectly(LoginRequest loginRequest)
+    {
+        var httpResponse = await HttpClient.PostAsJsonAsync("/auth/login", loginRequest);
+        var authenticationResponse = await httpResponse.Content.ReadFromJsonAsync<AuthenticationResponse>();
+
+        httpResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+        authenticationResponse.Should().NotBeNull();
+        authenticationResponse!.Token.Should().NotBeNullOrWhiteSpace();
+        authenticationResponse!.Email.Should().Be(loginRequest.Email);
+    }
+
+    /// <summary>
+    /// Checks if it returs a bad request response when the login credentials are invalid.
+    /// </summary>
+    /// <param name="loginRequest">The invalid request object.</param>
+    /// <param name="fieldsWithError">The fields that will contain some error.</param>
+    /// <returns>An asynchronous operation.</returns>
+    [Theory]
+    [MemberData(nameof(InvalidRequests))]
+    public async Task Login_WhenCredentialAreInvalid_ReturnsCorrectErrorResponse(
+        LoginRequest loginRequest,
+        params string[] fieldsWithError
+    )
+    {
+        var httpResponse = await HttpClient.PostAsJsonAsync("/auth/login", loginRequest);
+
+        httpResponse.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var errorResponse = await httpResponse.Content.ReadFromJsonAsync<HttpValidationProblemDetails>();
+
+        errorResponse.Should().NotBeNull();
+        errorResponse!.Status.Should().Be((int)HttpStatusCode.BadRequest);
+        errorResponse.Errors.Count.Should().Be(fieldsWithError.Length);
+
+        foreach (var field in fieldsWithError)
+        {
+            errorResponse.Errors[field].Should().NotBeEmpty();
+            errorResponse.Errors[field][0].Should().MatchRegex("^* must not be empty");
+        }
     }
 }
--- tests/IntegrationTests/TestUtils/Seeds/UserSeed.cs
+++ tests/IntegrationTests/TestUtils/Seeds/UserSeed.cs
@@ -9,10 +9,18 @@ namespace IntegrationTests.TestUtils.Seeds;
 /// </summary>
 public static class UserSeed
 {
+    /// <summary>
+    /// The admin password without passing through hashing.
+    /// </summary>
     public static readonly string AdminPassword = "admin123";
-
+    /// <summary>
+    /// The user password without passing through hashing. Used for both User1 and User2.
+    /// </summary>
     public static readonly string UserPassword = "user123";
 
+    /// <summary>
+    /// The administrator user. Contains the admin role.
+    /// </summary>
     public static readonly User Admin = UserUtils.CreateUser(
         name: "admin",
         roleId: Role.Admin.Id,
@@ -21,18 +29,28 @@ public static class UserSeed
         passwordSalt: "4FDE231393F2C8AECC2B26F356E3D89E"
     );
 
+    /// <summary>
+    /// A normal user with the customer role.
+    /// </summary>
     public static readonly User User1 = UserUtils.CreateUser(
         name: "user 1",
         email: "user1@email.com",
         roleId: Role.Customer.Id
     );
 
+    /// <summary>
+    /// A normal user with the customer role.
+    /// </summary>
     public static readonly User User2 = UserUtils.CreateUser(
         name: "user 2",
         email: "user2@email.com",
         roleId: Role.Customer.Id
     );
 
+    /// <summary>
+    /// List all the seed users.
+    /// </summary>
+    /// <returns>A list of all seed users.</returns>
     public static IEnumerable<User> List()
     {
         return [Admin, User1, User2];
