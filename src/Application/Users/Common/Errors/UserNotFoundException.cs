777f0291ad9112febb1212824ad202d0e0774723
ImVictorM <victorutdsim7@gmail.com>
2024-11-15 13:42:44 -0300

    fix: user not found exception error code

--- src/Application/Users/Common/Errors/UserNotFoundException.cs
+++ src/Application/Users/Common/Errors/UserNotFoundException.cs
@@ -7,12 +7,13 @@ namespace Application.Users.Common.Errors;
 /// </summary>
 public sealed class UserNotFoundException : BaseException
 {
-    private const string DefaultTitle = "User not found";
+    private const string DefaultTitle = "User Not Found";
+    private static readonly ErrorCode _defaultErrorCode = ErrorCode.NotFound;
 
     /// <summary>
     /// Initiates a new default instance of the <see cref="UserNotFoundException"/> class.
     /// </summary>
-    public UserNotFoundException() : base("The user being queried was not found", DefaultTitle)
+    public UserNotFoundException() : base("The user being queried was not found", DefaultTitle, _defaultErrorCode)
     {
     }
 
@@ -20,7 +21,7 @@ public sealed class UserNotFoundException : BaseException
     /// Initiates a new instance of the <see cref="UserNotFoundException"/> class with custom message.
     /// </summary>
     /// <param name="message">The exception message.</param>
-    public UserNotFoundException(string message) : base(message)
+    public UserNotFoundException(string message) : base(message, DefaultTitle, _defaultErrorCode)
     {
     }
 
@@ -29,7 +30,7 @@ public sealed class UserNotFoundException : BaseException
     /// </summary>
     /// <param name="message">The exception message.</param>
     /// <param name="innerException">The exception that caused the current exception.</param>
-    public UserNotFoundException(string message, Exception innerException) : base(message, innerException)
+    public UserNotFoundException(string message, Exception innerException) : base(message, DefaultTitle, _defaultErrorCode, innerException)
     {
     }
 }
