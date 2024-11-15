f1826d667429ce5ae162be3d8e7c016a6e71c5f4
ImVictorM <victorutdsim7@gmail.com>
2024-11-15 17:29:08 -0300

    feat: increment to lower snake case string extension

--- src/SharedKernel/Extensions/StringExtensions.cs
+++ src/SharedKernel/Extensions/StringExtensions.cs
@@ -12,8 +12,33 @@ public static class StringExtensions
     /// <returns>The string converted to snake_case and lowercase.</returns>
     public static string ToLowerSnakeCase(this string str)
     {
+        if (str.Equals(str.ToUpperInvariant(), StringComparison.Ordinal))
+        {
+            return str.ToLowerInvariant();
+        }
+
         return string
-            .Concat(str.Select((character, index) => index > 0 && char.IsUpper(character) ? $"_{character}" : $"{character}"))
+            .Concat(str.Select(ConvertChar))
             .ToLowerInvariant();
+    }
+
+    private static string ConvertChar(char character, int index)
+    {
+        if (index == 0)
+        {
+            return $"{character}";
+        }
+
+        if (char.IsUpper(character))
+        {
+            return $"_{character}";
+        }
+
+        if (character.Equals('-'))
+        {
+            return "_";
+        }
+
+        return $"{character}";
     }
 }
