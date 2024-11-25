c76af82478f2a899c0f8c0b962dd1cfda6977608
ImVictorM <victorutdsim7@gmail.com>
2024-11-25 19:09:48 -0300

    improve authentication logs

--- src/Application/Authentication/Commands/Register/RegisterCommandHandler.Logs.cs
+++ src/Application/Authentication/Commands/Register/RegisterCommandHandler.Logs.cs
@@ -6,55 +6,38 @@ public partial class RegisterCommandHandler
 {
     private readonly ILogger<RegisterCommandHandler> _logger;
 
-    /// <summary>
-    /// Log the start of the process to create a new user.
-    /// </summary>
-    /// <param name="email">The user email.</param>
     [LoggerMessage(
         EventId = 1,
-        Level = LogLevel.Information,
-        Message = "Initiating user creation with email {Email}"
+        Level = LogLevel.Debug,
+        Message = "Initiating user creation with email {Email}."
     )]
-    public partial void LogHandlingRegisterCommand(string email);
+    private partial void LogHandlingRegisterCommand(string email);
 
-    /// <summary>
-    /// Log a warning when trying to register a user that already exists.
-    /// </summary>
     [LoggerMessage(
         EventId = 2,
         Level = LogLevel.Warning,
         Message = "There was an error when trying to create a new user. The user already exists."
     )]
-    public partial void LogUserAlreadyExists();
+    private partial void LogUserAlreadyExists();
 
-    /// <summary>
-    /// Log an information saying that the user was instantiate successfully and associated with the customer role.
-    /// </summary>
+    [LoggerMessage(
+        EventId = 3,
+        Level = LogLevel.Debug,
+        Message = "User was created and associated with the customer role. Initiating persistence process."
+    )]
+    private partial void LogUserCreatedWithCustomerRole();
+
     [LoggerMessage(
         EventId = 4,
-        Level = LogLevel.Information,
-        Message = "User was created and associated with the customer role. Initiating persistence process"
+        Level = LogLevel.Debug,
+        Message = "User with email {Email} was saved successfully. Initiating token generation."
     )]
-    public partial void LogUserCreatedWithCustomerRole();
+    private partial void LogUserSavedSuccessfully(string email);
 
-    /// <summary>
-    /// Log an information saying that the user was successfully persisted in the database.
-    /// </summary>
-    /// <param name="email">The user email.</param>
     [LoggerMessage(
         EventId = 5,
-        Level = LogLevel.Information,
-        Message = "User with email {Email} was saved successfully. Initiating token generation"
+        Level = LogLevel.Debug,
+        Message = "Token generated successfully."
     )]
-    public partial void LogUserSavedSuccessfully(string email);
-
-    /// <summary>
-    /// Log an information saying that a token was generated successfully for the recent created user.
-    /// </summary>
-    [LoggerMessage(
-        EventId = 6,
-        Level = LogLevel.Information,
-        Message = "Token generated successfully"
-    )]
-    public partial void LogTokenGeneratedSuccessfully();
+    private partial void LogTokenGeneratedSuccessfully();
 }
--- src/Application/Authentication/Queries/Login/LoginQueryHandler.Logs.cs
+++ src/Application/Authentication/Queries/Login/LoginQueryHandler.Logs.cs
@@ -6,46 +6,31 @@ public partial class LoginQueryHandler
 {
     private readonly ILogger<LoginQueryHandler> _logger;
 
-    /// <summary>
-    /// Log the initialization of the login process.
-    /// </summary>
-    /// <param name="email">The email of the user trying to login.</param>
     [LoggerMessage(
         EventId = 1,
-        Level = LogLevel.Information,
-        Message = "Handling login for user with email {Email}"
+        Level = LogLevel.Debug,
+        Message = "Handling login for user with email {Email}."
     )]
-    public partial void LogHandlingLoginQuery(string email);
+    private partial void LogHandlingLoginQuery(string email);
 
-    /// <summary>
-    /// Log a warning when the user trying to login is not found using their email.
-    /// </summary>
     [LoggerMessage(
         EventId = 2,
-        Level = LogLevel.Warning,
-        Message = "Authentication failed: Incorrect credentials supplied or user may be inactive"
+        Level = LogLevel.Debug,
+        Message = "Authentication failed: Incorrect credentials supplied or user may be inactive."
     )]
-    public partial void LogAuthenticationFailed();
+    private partial void LogAuthenticationFailed();
 
-
-    /// <summary>
-    /// Log an information when the user email and password was correct.
-    /// </summary>
-    /// <param name="email">The user email.</param>
     [LoggerMessage(
-        EventId = 4,
-        Level = LogLevel.Information,
+        EventId = 3,
+        Level = LogLevel.Debug,
         Message = "User with email {Email} was successfully authenticated. Initiating token generation."
     )]
-    public partial void LogSuccessfullyAuthenticatedUser(string email);
+    private partial void LogSuccessfullyAuthenticatedUser(string email);
 
-    /// <summary>
-    /// Log an information saying that the token was successfully generated for the user.
-    /// </summary>
     [LoggerMessage(
-        EventId = 5,
-        Level = LogLevel.Information,
-        Message = "JWT token generated successfully"
+        EventId = 4,
+        Level = LogLevel.Debug,
+        Message = "JWT token generated successfully."
     )]
-    public partial void LogTokenGenerated();
+    private partial void LogTokenGenerated();
 }
