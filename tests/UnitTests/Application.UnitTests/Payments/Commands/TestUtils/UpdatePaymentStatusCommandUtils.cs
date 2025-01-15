e880336c6fd0f7e54a269e7ecb2514036b882f4d
Victor Mendes <victorutdsim7@gmail.com>
2025-01-15 10:19:24 -0300

    test: unit tests for the update payment status feature

---
+++ tests/UnitTests/Application.UnitTests/Payments/Commands/TestUtils/UpdatePaymentStatusCommandUtils.cs
@@ -0,0 +1,29 @@
+using Application.Payments.Commands.UpdatePaymentStatus;
+
+using Domain.PaymentAggregate.Enumerations;
+using Domain.UnitTests.TestUtils;
+
+namespace Application.UnitTests.Payments.Commands.TestUtils;
+
+/// <summary>
+/// Utilities for the <see cref="UpdatePaymentStatusCommand"/> class.
+/// </summary>
+public static class UpdatePaymentStatusCommandUtils
+{
+    /// <summary>
+    /// Creates a new instance of the <see cref="UpdatePaymentStatusCommand"/> class.
+    /// </summary>
+    /// <param name="paymentId">The payment id.</param>
+    /// <param name="status">The payment status.</param>
+    /// <returns>A new instance of the <see cref="UpdatePaymentStatusCommand"/> class.</returns>
+    public static UpdatePaymentStatusCommand CreateCommand(
+        string? paymentId = null,
+        string? status = null
+    )
+    {
+        return new UpdatePaymentStatusCommand(
+            paymentId ?? NumberUtils.CreateRandomLongAsString(),
+            status ?? PaymentStatus.Pending.Name
+        );
+    }
+}
