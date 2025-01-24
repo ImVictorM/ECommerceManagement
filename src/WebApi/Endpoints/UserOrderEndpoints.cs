d670f8e7d1068fa48b30ac80af4a4d5f87dd3b5e
Victor Mendes <victorutdsim7@gmail.com>
2025-01-23 21:58:26 -0300

    docs: (requests) add order feature docs an refine product docs

--- src/WebApi/Endpoints/UserOrderEndpoints.cs
+++ src/WebApi/Endpoints/UserOrderEndpoints.cs
@@ -21,7 +21,7 @@ public sealed class UserOrderEndpoints : ICarterModule
     {
         var userOrderGroup = app
             .MapGroup("users/{userId:long}/orders")
-            .WithTags("UserOrders")
+            .WithTags("Orders")
             .WithOpenApi();
 
         userOrderGroup
