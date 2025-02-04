90a8982dfa8891b6fa8af0a8a8776cbdd4e2d14d
Victor Mendes <victorutdsim7@gmail.com>
2025-02-04 16:11:36 -0300

    feat: improve carrier model

--- src/Domain/CarrierAggregate/Carrier.cs
+++ src/Domain/CarrierAggregate/Carrier.cs
@@ -1,6 +1,7 @@
 using Domain.CarrierAggregate.ValueObjects;
 
 using SharedKernel.Models;
+using SharedKernel.ValueObjects;
 
 namespace Domain.CarrierAggregate;
 
@@ -13,21 +14,39 @@ public sealed class Carrier : AggregateRoot<CarrierId>
     /// Gets the carrier name.
     /// </summary>
     public string Name { get; private set; } = null!;
+    /// <summary>
+    /// Gets the carrier email.
+    /// </summary>
+    public Email Email { get; private set; } = null!;
+    /// <summary>
+    /// Gets the carrier password hash.
+    /// </summary>
+    public PasswordHash PasswordHash { get; private set; } = null!;
+    /// <summary>
+    /// Gets the carrier phone.
+    /// </summary>
+    public string? Phone { get; private set; }
 
     private Carrier() { }
 
-    private Carrier(string name)
+    private Carrier(string name, Email email, PasswordHash passwordHash, string? phone = null)
     {
         Name = name;
+        Email = email;
+        PasswordHash = passwordHash;
+        Phone = phone;
     }
 
     /// <summary>
     /// Creates a new instance of the <see cref="Carrier"/> class.
     /// </summary>
     /// <param name="name">The carrier name.</param>
+    /// <param name="email">The carrier email.</param>
+    /// <param name="passwordHash">The carrier password hash.</param>
+    /// <param name="phone">The carrier phone.</param>
     /// <returns>A new instance of the <see cref="Carrier"/> class.</returns>
-    public static Carrier Create(string name)
+    public static Carrier Create(string name, Email email, PasswordHash passwordHash, string? phone = null)
     {
-        return new Carrier(name);
+        return new Carrier(name, email, passwordHash, phone);
     }
 }
--- src/Infrastructure/Carriers/CarrierConfigurations.cs
+++ src/Infrastructure/Carriers/CarrierConfigurations.cs
@@ -1,17 +1,57 @@
+using Application.Common.Security.Authentication;
+
 using Domain.CarrierAggregate;
 using Domain.CarrierAggregate.ValueObjects;
 
+using Infrastructure.Common.Persistence.Configurations.Abstracts;
+
+using SharedKernel.ValueObjects;
+
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Carriers;
 
 /// <summary>
 /// Configures the tables for the <see cref="Carrier"/> aggregate.
 /// </summary>
-public class CarrierConfigurations : IEntityTypeConfiguration<Carrier>
+public class CarrierConfigurations : EntityTypeConfigurationDependency<Carrier>
 {
+    private sealed record CarrierData(
+        CarrierId Id,
+        string Name,
+        Email Email,
+        PasswordHash PasswordHash,
+        string? Phone,
+        DateTimeOffset CreatedAt,
+        DateTimeOffset UpdatedAt
+    );
+
+    private readonly CarrierData _defaultInternalCarrier;
+
+    /// <summary>
+    /// Initiates a new instance of the <see cref="CarrierConfigurations"/> class.
+    /// </summary>
+    /// <param name="carrierOptions">The default internal carrier options.</param>
+    /// <param name="passwordHasher">The password hasher to hash the carrier password.</param>
+    public CarrierConfigurations(IOptions<CarrierInternalSettings> carrierOptions, IPasswordHasher passwordHasher)
+    {
+        var carrierSettings = carrierOptions.Value;
+
+        _defaultInternalCarrier = new CarrierData(
+            CarrierId.Create(1),
+            carrierSettings.Name,
+            Email.Create(carrierSettings.Email),
+            passwordHasher.Hash(carrierSettings.Password),
+            carrierSettings.Phone,
+            DateTimeOffset.UtcNow,
+            DateTimeOffset.UtcNow
+        );
+    }
+
     /// <inheritdoc/>
-    public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Carrier> builder)
+    public override void Configure(EntityTypeBuilder<Carrier> builder)
     {
         builder.ToTable("carriers");
 
@@ -27,5 +67,26 @@ public class CarrierConfigurations : IEntityTypeConfiguration<Carrier>
             .Property(c => c.Name)
             .HasMaxLength(120)
             .IsRequired();
+
+        builder
+            .Property(c => c.Email)
+            .HasConversion(email => email.Value, value => Email.Create(value))
+            .HasMaxLength(120)
+            .IsRequired();
+
+        builder
+            .Property(c => c.PasswordHash)
+            .HasConversion(hash => hash.Value, value => PasswordHash.Create(value))
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder
+            .Property(c => c.Phone)
+            .HasMaxLength(11)
+            .IsRequired(false);
+
+        builder.HasIndex(c => c.Email).IsUnique();
+
+        builder.HasData(_defaultInternalCarrier);
     }
 }
