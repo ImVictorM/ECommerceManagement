005d2819a84394180e0e41332c3e455addd23edd
Victor Mendes <victorutdsim7@gmail.com>
2025-02-21 20:29:19 -0300

    fix: order repository projections

--- src/Infrastructure/Orders/OrderRepository.cs
+++ src/Infrastructure/Orders/OrderRepository.cs
@@ -3,8 +3,6 @@ using Application.Orders.DTOs;
 
 using Domain.OrderAggregate;
 using Domain.OrderAggregate.ValueObjects;
-using Domain.PaymentAggregate.ValueObjects;
-using Domain.ShippingMethodAggregate.ValueObjects;
 using Domain.UserAggregate.ValueObjects;
 
 using Infrastructure.Common.Persistence;
@@ -27,6 +25,7 @@ public sealed class OrderRepository : BaseRepository<Order, OrderId>, IOrderRepo
     }
 
     /// <inheritdoc/>
+    /// <inheritdoc/>
     public async Task<OrderDetailedQueryResult?> GetOrderDetailedAsync(
         OrderId orderId,
         UserId ownerId,
@@ -37,8 +36,26 @@ public sealed class OrderRepository : BaseRepository<Order, OrderId>, IOrderRepo
             .Where(order => order.Id == orderId && order.OwnerId == ownerId)
             .Select(order => new OrderDetailedQueryResult(
                 order,
-                GetOrderShipmentResult(orderId),
-                GetOrderPaymentId(orderId)
+                Context.Shipments
+                    .Where(shipment => shipment.OrderId == orderId)
+                    .Select(shipment => new OrderShipmentResult(
+                        shipment.Id,
+                        shipment.ShipmentStatus.Name,
+                        shipment.DeliveryAddress,
+                        Context.ShippingMethods
+                            .Where(method => method.Id == shipment.ShippingMethodId)
+                            .Select(method => new OrderShippingMethodResult(
+                                method.Name,
+                                method.Price,
+                                method.EstimatedDeliveryDays
+                            ))
+                            .First()
+                    ))
+                    .First(),
+                Context.Payments
+                    .Where(payment => payment.OrderId == orderId)
+                    .Select(payment => payment.Id)
+                    .First()
             ))
             .FirstOrDefaultAsync(cancellationToken);
     }
@@ -53,42 +70,27 @@ public sealed class OrderRepository : BaseRepository<Order, OrderId>, IOrderRepo
             .Where(order => order.Id == orderId)
             .Select(order => new OrderDetailedQueryResult(
                 order,
-                GetOrderShipmentResult(orderId),
-                GetOrderPaymentId(orderId)
+                Context.Shipments
+                    .Where(shipment => shipment.OrderId == orderId)
+                    .Select(shipment => new OrderShipmentResult(
+                        shipment.Id,
+                        shipment.ShipmentStatus.Name,
+                        shipment.DeliveryAddress,
+                        Context.ShippingMethods
+                            .Where(method => method.Id == shipment.ShippingMethodId)
+                            .Select(method => new OrderShippingMethodResult(
+                                method.Name,
+                                method.Price,
+                                method.EstimatedDeliveryDays
+                            ))
+                            .First()
+                    ))
+                    .First(),
+                Context.Payments
+                    .Where(payment => payment.OrderId == orderId)
+                    .Select(payment => payment.Id)
+                    .First()
             ))
             .FirstOrDefaultAsync(cancellationToken);
-    }
-
-    private OrderShipmentResult GetOrderShipmentResult(OrderId orderId)
-    {
-        return Context.Shipments
-            .Where(shipment => shipment.OrderId == orderId)
-            .Select(shipment => new OrderShipmentResult(
-                shipment.Id,
-                shipment.ShipmentStatus.Name,
-                shipment.DeliveryAddress,
-                GetOrderShipmentShippingMethodResult(shipment.ShippingMethodId)
-            ))
-            .First();
-    }
-
-    private OrderShippingMethodResult GetOrderShipmentShippingMethodResult(ShippingMethodId shippingMethodId)
-    {
-        return Context.ShippingMethods
-            .Where(method => method.Id == shippingMethodId)
-            .Select(method => new OrderShippingMethodResult(
-                method.Name,
-                method.Price,
-                method.EstimatedDeliveryDays
-            ))
-            .First();
-    }
-
-    private PaymentId GetOrderPaymentId(OrderId orderId)
-    {
-        return Context.Payments
-            .Where(payment => payment.OrderId == orderId)
-            .Select(payment => payment.Id)
-            .First();
     }
 }
