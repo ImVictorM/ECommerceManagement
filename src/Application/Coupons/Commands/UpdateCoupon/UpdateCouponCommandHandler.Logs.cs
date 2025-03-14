dbcc733c40a2bdf9f5126402d00cd223810f6fd8
Victor Mendes <victorutdsim7@gmail.com>
2025-03-14 17:47:39 -0300

    refactor: update coupon log level

--- src/Application/Coupons/Commands/UpdateCoupon/UpdateCouponCommandHandler.Logs.cs
+++ src/Application/Coupons/Commands/UpdateCoupon/UpdateCouponCommandHandler.Logs.cs
@@ -22,28 +22,28 @@ internal sealed partial class UpdateCouponCommandHandler
 
     [LoggerMessage(
         EventId = 3,
-        Level = LogLevel.Information,
+        Level = LogLevel.Debug,
         Message = "The coupon base info was updated."
     )]
     private partial void LogCouponUpdated();
 
     [LoggerMessage(
         EventId = 4,
-        Level = LogLevel.Information,
+        Level = LogLevel.Debug,
         Message = "All the coupon former restrictions were cleared."
     )]
     private partial void LogCouponFormerRestrictionsCleared();
 
     [LoggerMessage(
         EventId = 5,
-        Level = LogLevel.Information,
+        Level = LogLevel.Debug,
         Message = "Assigning new restriction of type {RestrictionType}."
     )]
     private partial void LogAssigningNewRestriction(string restrictionType);
 
     [LoggerMessage(
         EventId = 6,
-        Level = LogLevel.Information,
+        Level = LogLevel.Debug,
         Message = "All new restrictions assigned successfully."
     )]
     private partial void LogRestrictionsAssigned();
